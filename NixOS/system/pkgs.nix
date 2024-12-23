ab871f730bd607c0f4f4d43010a5a51cf406b553
b65t <iislamm6s7s5@tutamail.com>
2024-12-23 03:14:00 +0100

    Add files via upload

---
+++ NixOS/system/pkgs.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+   environment.systemPackages = with pkgs; [
+
+   fish
+   python3
+   pavucontrol
+   git
+
+   ];
+
+}
