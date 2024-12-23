2b1eb3cded0ab342f8bf55e4f608188c460f17f3
b65t <iislamm6s7s5@tutamail.com>
2024-12-23 03:12:20 +0100

    Delete home directory

--- home/desktop/wayland/waybar/play-next.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-spotify_status=$(playerctl --player=spotify status 2>/dev/null)
-
-if [ "$spotify_status" == "Playing" ]; then
-    playerctl --player=spotify next
-else
-    playerctl --player=musikcube next
-fi
--- home/desktop/x11/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  imports = [
-    ./i3
-    ./picom
-    ./polybar
-  ];
-}
