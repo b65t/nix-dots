3d955d579e7fb97a897f194e8877cc2531238af9
b65t <iislamm6s7s5@tutamail.com>
2024-12-24 03:58:44 +0100

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -5,14 +5,38 @@
 
    inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    spicetify-nix = {
+        url = "github:Gerg-L/spicetify-nix";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+     nixcord = {
+        url = "github:kaylorben/nixcord";
+    };
+
+     textfox = {
+        url = "github:adriankarlen/textfox";
+    };
    };
 
-   outputs = { self, nixpkgs, home-manager, ... }:
+   outputs =
+     inputs @ {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+       }:
      let
        lib = nixpkgs.lib;
        system = "x86_64-linux";
+       hostname = "islamflake";
+       username = "islam";
        pkgs = import nixpkgs {
           inherit system;
 	     config.allowUnfree = true;	
@@ -20,16 +44,32 @@
      in {
         nixosConfigurations = {
           nixos = nixpkgs.lib.nixosSystem {
+	  specialArgs = {
             inherit system;
+            inherit username;
+            inherit hostname;
+            inherit inputs;
+	};
             modules = [ ./NixOS/configuration.nix ];
       };
     };
     homeConfigurations = {
     islam = home-manager.lib.homeManagerConfiguration {
-    inherit pkgs;
-    modules = [ ./home/home.nix ];
+    pkgs = pkgs;
+    extraSpecialArgs = {
+     inherit pkgs;
+     inherit username;
+     inherit hostname;
+     inherit system;
+     inherit inputs;
+    };
+    modules = [ 
+    ./home/home.nix
+    inputs.spicetify-nix.homeManagerModules.default
+    inputs.nixcord.homeManagerModules.nixcord
+    inputs.textfox.homeManagerModules.default
+    ];
    };
   };
  };
-
 }
