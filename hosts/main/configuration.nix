261681be8b139f47cee11a31d674ab96e25f08fc
b65t <iislamm6s7s5@tutamail.com>
2025-01-03 23:23:22 +0100

    Add files via upload

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -66,7 +66,7 @@
 
   # Enable sound.
   # sound.enable = true;
-   hardware.pulseaudio.enable = false;
+   services.pulseaudio.enable = false;
    security.rtkit.enable = true;
    services.pipewire = {
      enable = true;
